9446add8eecc83bb7efaeb310d76296aed879574
iamtor9 <iamtor9@gmail.com>
2019-10-15 11:28:21 -0500

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Cards from "./components/Cards/Cards"
+import Head from "./components/Head/Head";
+import Wrapper from "./components/Wrapper/Wrapper";
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
