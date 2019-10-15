42bc08817b6a3ac4f7ff7b3efe27c9ecbc6cdb25
iamtor9 <iamtor9@gmail.com>
2019-10-15 11:26:37 -0500

    revamped app.js

--- src/app.js
+++ src/app.js
@@ -1,61 +1,100 @@
 // create imports for all js related files
-import React, { useState } from "react";
-import ReactDOMServer from 'react-dom/server';
-import Cards from "./components/Cards";
-import wrapper from "./components/wrapper";
-import Head from "./components/Head";
-import Cards from "../src/Cards.json";
-import "./App.css";
+import React, { Component } from "react";
+import Cards from "./components/Cards/Cards"
+import Head from "./components/Head/Head";
+import Wrapper from "./components/Wrapper/Wrapper";
+// import Cards from "../src/Cards.json";
 
+class App extends Component {
+  state = {
+    Cards
+  };
 
-function App () {
+  removeCard = id => {
+    const Cards = this.state.Cards.filter(Card => Card.id !== id);
+    this.setState({ Cards });
+  };
 
-const [cardsState, setcardsState] = useState(cards); 
-const [scoreState, setScoreState] = useState(0);
-const [highScoreState, setHighScoreState] = useState(0);
-const [answerState, setAnswerState] = useState("Click an image to start!")
-
-function clickCount(id) {
-
-    return cardsState.find((o, i) => {
-      if (o.id === id) {
-        if (cards[i].count === 0) {
-          cards[i].count = cards[i].count + 1;
-          setScoreState(scoreState + 1)
-          setAnswerState("Yay You Did It!")
-          setcardsState(cardsState.sort(() => Math.random() - 0.5))
-        } else {
-          if (scoreState > highScoreState) {
-          setHighScoreState(scoreState)
-          setAnswerState("You guessed incorrectly!")
-          }
-          cardsState.forEach(cards => {
-          cards.count = 0;
-          });
-          alert(`Game Over :( \nscore: ${scoreState}`);
-      
-          setScoreState(0)
-          setcardsState(cardsState.sort(() => Math.random() - 0.5))
-        }
-        return true;
-      }
-    });
-  }
-
-    return (
-      <wrapper>
-        <Head answer={answerState} score={scoreState} highscore={highScoreState}>Clicky Click Scoreboard!</Head>
-        {cardsState.map(Cards => (
-          <Cards
-            clickCount={clickCount}
-            id={Cards.id}
-            key={Cards.id}
-            image={Cards.image}
-          /> 
+  render() {
+    return ( 
+      <Wrapper>
+        <Head>Cards List</Head>
+        {this.state.Cards.map(Card => (
+          <CardCard
+          removeCard={this.removeCard}
+          id={Card.id}
+          key={Card.id}
+          image={Card.image}
+          />
         ))}
-      </wrapper>
-    );
-  
+      </Wrapper>
+    )
+  }
 }
-
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function App () {
+
+// const [cardsState, setcardsState] = Component(cards); 
+// const [scoreState, setScoreState] = Component(0);
+// const [highScoreState, setHighScoreState] = Component(0);
+// const [answerState, setAnswerState] = Component("Click an image to start!")
+
+// function clickCount(id) {
+
+//     return cardsState.find((o, i) => {
+//       if (o.id === id) {
+//         if (cards[i].count === 0) {
+//           cards[i].count = cards[i].count + 1;
+//           setScoreState(scoreState + 1)
+//           setAnswerState("Yay You Did It!")
+//           setcardsState(cardsState.sort(() => Math.random() - 0.5))
+//         } else {
+//           if (scoreState > highScoreState) {
+//           setHighScoreState(scoreState)
+//           setAnswerState("You guessed incorrectly!")
+//           }
+//           cardsState.forEach(cards => {
+//           cards.count = 0;
+//           });
+//           alert(`Game Over :( \nscore: ${scoreState}`);
+      
+//           setScoreState(0)
+//           setcardsState(cardsState.sort(() => Math.random() - 0.5))
+//         }
+//         return true;
+//       }
+//     });
+//   }
+
+//     return (
+//       <wrapper>
+//         <Head answer={answerState} score={scoreState} highscore={highScoreState}>Clicky Click Scoreboard!</Head>
+//         {cardsState.map(Cards => (
+//           <Cards
+//             clickCount={clickCount}
+//             id={Cards.id}
+//             key={Cards.id}
+//             image={Cards.image}
+//           /> 
+//         ))}
+//       </wrapper>
+//     );
+  
+// }
+
+// export default App;
